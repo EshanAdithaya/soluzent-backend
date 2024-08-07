d197544c57d58b2dca6acd4120f1d528ad6410a6
Thisitha Kavinda <31003329+thisitha@users.noreply.github.com>
2024-08-08 01:42:35 +0530

    dev open ai

--- src/openai-facebook/openai-facebook.controller.ts
+++ src/openai-facebook/openai-facebook.controller.ts
@@ -1,25 +1,25 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
-import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { OpenaiFacebookService } from './openai-facebook.service';
 
 @Controller('openAI-facebook')
 @ApiTags('openAI-facebook')
 export class OpenaiFacebookController {
   constructor(private readonly openaiService: OpenaiFacebookService) {}
-  @Get('query/:query')
-  @ApiOperation({ summary: 'Query analytics data' })
-  @ApiParam({
-    name: 'query',
-    description: 'The analytics query',
-    type: 'string',
-  })
-  @ApiResponse({
-    status: 200,
-    description: 'Returns the analyzed query results',
-  })
-  async queryAnalytics(@Param('query') query: string) {
-    return this.openaiService.runConversation(query);
-  }
+  // @Get('query/:query')
+  // @ApiOperation({ summary: 'Query analytics data' })
+  // @ApiParam({
+  //   name: 'query',
+  //   description: 'The analytics query',
+  //   type: 'string',
+  // })
+  // @ApiResponse({
+  //   status: 200,
+  //   description: 'Returns the analyzed query results',
+  // })
+  // async queryAnalytics(@Param('query') query: string) {
+  //   return this.openaiService.runConversation(query);
+  // }
   @Get('queryPost/:query')
   @ApiOperation({ summary: 'Query analytics data' })
   @ApiResponse({
